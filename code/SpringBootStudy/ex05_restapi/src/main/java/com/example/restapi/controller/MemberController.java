390c184b4221ba0dac9f6637cdbbf6c908d8fae4
sjungwon03 <sjungwon03@gmail.com>
2026-06-09 11:49:00 +0900

    docs: spring boot 실습

--- code/SpringBootStudy/ex05_restapi/src/main/java/com/example/restapi/controller/MemberController.java
+++ code/SpringBootStudy/ex05_restapi/src/main/java/com/example/restapi/controller/MemberController.java
@@ -9,10 +9,19 @@ import com.example.restapi.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -31,6 +40,42 @@ public class MemberController {
   }
 
   // 2. 전체 조회
+  @GetMapping
+  public ResponseEntity<List<MemberResponse>> getAllMembers(@RequestParam String param) {
+    return ResponseEntity.ok(memberService.findAll());
+  }
   
+  // 3. 단건 조회
+  @GetMapping("/{id}")
+  public ResponseEntity<MemberResponse> getMemberById(@PathVariable("id") Long id) {
+    try {
+      MemberResponse foundMember = memberService.findById(id);
+      return ResponseEntity.ok(foundMember);
+    }catch(Exception e){
+      return ResponseEntity.notFound().build();
+    }
+  }
   
+  // 4. 수정
+  @PutMapping("/{id}")
+  public ResponseEntity<MemberResponse> update(
+    @PathVariable Long id,
+    @RequestBody MemberRequest request) {
+      try{
+        MemberResponse updatedMember = memberService.update(id, request);
+        return ResponseEntity.ok(updatedMember);
+      }catch(Exception e){
+        return ResponseEntity.notFound().build();
+      }
+  }
+
+  @DeleteMapping
+  public ResponseEntity<Void> deleteMember(@PathVariable("id") Long id){
+    try {
+      memberService.delete(id);
+      return ResponseEntity.noContent().build();
+    }catch(Exception e){
+      return ResponseEntity.notFound().build();
+    }
+  }
 }
